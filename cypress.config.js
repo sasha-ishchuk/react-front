f82fd8a88827168fb68db79e84b0613d99210d16
sasha <alexa.ishchuk@gmail.com>
2024-08-17 18:44:48 +0200

    Task 7 - reliability fixes (sonar)

--- cypress.config.js
+++ cypress.config.js
@@ -8,13 +8,6 @@ module.exports = defineConfig({
     },
   },
 
-  component: {
-    devServer: {
-      framework: "create-react-app",
-      bundler: "webpack",
-    },
-  },
-
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
